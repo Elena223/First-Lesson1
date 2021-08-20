a1213498c009e8d54c6f367afc53b3da6e944993
Elena Popa <elenapopa369@gmail.com>
2021-08-20 07:55:34 +0300

    Add project files.

---
+++ First Lesson/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace First_Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Your name is: {}", name) ;
+            
+        }
+    }
+}
